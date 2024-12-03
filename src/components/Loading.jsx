166515d059069318e59288b66c7a875790f90553
liltexh <liltexhnology@gmail.com>
2024-12-03 20:17:38 +0100

    reduced opacity for loading screen

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -5,7 +5,7 @@ export default function Loading() {
 			src={loading}
 			alt="loading img"
 			width="96rem"
-			className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "
+			className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-65"
 		/>
 	);
 }
