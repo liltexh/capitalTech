ab5ece2567dcb8c80c41edc3e5eae14541d9615c
liltexh <liltexhnology@gmail.com>
2024-12-06 21:33:57 +0100

    added design to payment button

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -4,7 +4,7 @@ import { infoIcon, bankIcon, cardIcon, cryptoIcon } from "../Tools/icons";
 export default function CheckOut() {
 	const [page, setPage] = useState(1);
 	return (
-		<main className="w-full max-h-screen flex flex-col justify-center items-center p-6 pt-24 m-auto gap-10">
+		<main className="w-full min-h-screen flex flex-col justify-center items-center px-6 pt-24 m-auto gap-10 ">
 			<div className="flex gap-8">
 				<span
 					className={`relative w-6 scale-150 text-center rounded-full  ${
@@ -30,7 +30,7 @@ export default function CheckOut() {
 			</div>
 			<div>
 				{page == 1 && (
-					<div className="front_blur_background flex flex-col justify-center items-center gap-4 p-4 glowing_shadow rounded-md max-w-[40rem]">
+					<div className="front_blur_background flex flex-col justify-center items-center gap-6 px-4 py-6 glowing_shadow rounded-md max-w-[40rem]">
 						<h1 className="text-2xl font-bold text-amber-400 text-center">
 							Your Financial Journey Begins Here
 						</h1>
@@ -44,7 +44,7 @@ export default function CheckOut() {
 								/>
 							</label>
 							<div className="flex justify-between">
-								<div className="flex flex-col justify-center  rounded border-r-2 border-amber-400 w-28">
+								<div className="flex flex-col justify-center  rounded border-r-2 border-amber-400 w-28 h-12 p-2">
 									<span className="text-xs font-semibold text-amber-400 self-start">
 										parcent Intrest :
 									</span>
@@ -55,7 +55,7 @@ export default function CheckOut() {
 										</span>
 									</div>
 								</div>
-								<div className="flex flex-col justify-center border-r-2  border-amber-400 w-28 rounded">
+								<div className="flex flex-col justify-center border-r-2  border-amber-400 w-28 rounded h-12 p-2">
 									<span className="text-xs font-semibold text-amber-400 ">
 										Frequncy :
 									</span>
@@ -81,31 +81,54 @@ export default function CheckOut() {
 								</p>
 							</div>
 						</div>
-						<div className="flex justify-center items-center gap-4">
-							<span className="">
-								<img
-									src={cardIcon}
-									alt="card icon"
-									width="30px"
-									className=""
-								/>
-								<p>Card</p>
+						<div className="inline-flex justify-center items-center gap-10 mt-8">
+							<span className="flex flex-col justify-center items-center relative z-10 group">
+								<span className=" flex h-14 w-14 bg-gray-700 group-hover:bg-amber-400 group-hover:shadow-box-shadow rounded-full text-center justify-center items-center cursor-pointer relative z-10">
+									<img
+										src={cardIcon}
+										alt="card icon"
+										width="30px"
+										className=""
+									/>
+								</span>
+								<p
+									className="text-amber-400 px-2 font-semibold absolute top-0 rounded border-b-[3px] border-b-amber-400 opacity-0 group-hover:opacity-100 group-hover:-top-10 pointer-events-none group-hover:pointer-events-auto before:absolute before:content-['']
+								before:w-3 before:h-3 before:left-[50%] before:bg-amber-400 before:-bottom-2 before:transform before:-translate-x-[50%] before:rotate-[135deg] before:clip-path-diagonal-slice  transition-all duration-500 ease-[cubic-bezier(0.68,-0.55,0.265,1.55)]"
+								>
+									Card
+								</p>
 							</span>
-							<span>
-								<img
-									src={bankIcon}
-									alt="card icon"
-									width="30px"
-								/>
-								<p>Bank</p>
+							<span className="flex flex-col justify-center items-center relative z-10 group">
+								<span className=" flex h-14 w-14 bg-gray-700 group-hover:bg-amber-400 group-hover:shadow-box-shadow rounded-full text-center justify-center items-center cursor-pointer relative z-10">
+									<img
+										src={bankIcon}
+										alt="bank icon"
+										width="25px"
+										className=""
+									/>
+								</span>
+								<p
+									className="text-amber-400 px-2 font-semibold absolute top-0 rounded border-b-[3px] border-b-amber-400 opacity-0 group-hover:opacity-100 group-hover:-top-10 pointer-events-none group-hover:pointer-events-auto before:absolute before:content-['']
+								before:w-3 before:h-3 before:left-[50%] before:bg-amber-400 before:-bottom-2 before:transform before:-translate-x-[50%] before:rotate-[135deg] before:clip-path-diagonal-slice  transition-all duration-500 ease-[cubic-bezier(0.68,-0.55,0.265,1.55)]"
+								>
+									Bank
+								</p>
 							</span>
-							<span>
-								<img
-									src={cryptoIcon}
-									alt="card icon"
-									width="30px"
-								/>
-								Crypto
+							<span className="flex flex-col justify-center items-center relative z-10 group">
+								<span className=" flex h-14 w-14 bg-gray-700 group-hover:bg-amber-400 group-hover:shadow-box-shadow rounded-full text-center justify-center items-center cursor-pointer relative z-10">
+									<img
+										src={cryptoIcon}
+										alt="crypto icon"
+										width="28px"
+										className=""
+									/>
+								</span>
+								<p
+									className="text-amber-400 px-2 font-semibold absolute top-0 rounded border-b-[3px] border-b-amber-400 opacity-0 group-hover:opacity-100 group-hover:-top-10 pointer-events-none group-hover:pointer-events-auto before:absolute before:content-['']
+								before:w-3 before:h-3 before:left-[50%] before:bg-amber-400 before:-bottom-2 before:transform before:-translate-x-[50%] before:rotate-[135deg] before:clip-path-diagonal-slice  transition-all duration-500 ease-[cubic-bezier(0.68,-0.55,0.265,1.55)]"
+								>
+									Crypto
+								</p>
 							</span>
 						</div>
 					</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
-
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			clipPath: {
+				"diagonal-slice": "polygon(0 0,100% 0 ,100% 100%)",
+			},
+			boxShadow: {
+				"box-shadow": "0 0 16px rgba(240, 203, 70, 0.7)",
+			},
+		},
+	},
+	plugins: [require("tailwind-clip-path")],
+};
